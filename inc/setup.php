460ab14e7866be75a3013cd9c66fcf9dafaf07c4
allensph <allensph@gmail.com>
2017-05-15 17:11:02 +0800

    修正 archieve title
    移除文章彙整中的"分類："

--- inc/setup.php
+++ inc/setup.php
@@ -123,3 +123,24 @@ if ( ! function_exists( 'all_excerpts_get_more_link' ) ) {
 	}
 }
 add_filter( 'wp_trim_excerpt', 'all_excerpts_get_more_link' );
+
+
+add_filter( 'get_the_archive_title', function ($title) {
+
+    if ( is_category() ) {
+
+            $title = single_cat_title( '', false );
+
+        } elseif ( is_tag() ) {
+
+            $title = single_tag_title( '', false );
+
+        } elseif ( is_author() ) {
+
+            $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+
+        }
+
+    return $title;
+
+});
